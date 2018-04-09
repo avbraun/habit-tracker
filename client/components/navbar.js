bfb7ce14a6948542931526e7b41df0d8d32543f3
Alyssa Venere Braun <alyssavenere@gmail.com>
2018-04-09 18:44:59 -0500

    set up

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>HABIT TRACKER</h1>
     <nav>
       {isLoggedIn ? (
         <div>
